9997214b7595ae31d72fb6d658600d64fc73b724
Haedon Kaufman <haka9670@gmail.com>
2022-06-14 22:55:14 -0700

    add files to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const {
-  NODE_ENV = "development",
+  NODE_ENV = "production",
   DEVELOPMENT_DATABASE_URL,
   PRODUCTION_DATABASE_URL,
 } = process.env;
+console.log(NODE_ENV)
 const URL =
   NODE_ENV === "production"
     ? PRODUCTION_DATABASE_URL
     : DEVELOPMENT_DATABASE_URL;
+
+    console.log(URL)
 
 module.exports = {
   development: {
